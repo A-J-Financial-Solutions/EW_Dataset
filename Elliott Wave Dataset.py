fb9be99409debc30e2d0674f7ecd1512d3ed7bf3
anthony <qiko0425@gmail.com>
2024-03-02 19:41:59 -0500

    Add python script

--- Elliott Wave Dataset.py
+++ Elliott Wave Dataset.py
@@ -4,10 +4,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
 # %%
-stock_symbol = 'DJI'
-start_date = '2009-03-09'
-end_date = '2010-04-26'
+# Change the stock symbol and date range
+stock_symbol = 'SPY'
+start_date = '2018-12-24'
+end_date = '2020-02-19'
 
 stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
 
@@ -20,12 +22,15 @@ plt.plot(stock_data['Close'])
 plt.title(f'{stock_symbol} Close Price')
 plt.xlabel('Date')
 plt.ylabel('Close Price')
-plt.savefig(f'./mnt/data/{stock_symbol}_{start_date}_to_{end_date}.png')
+plt.savefig(f'./mnt/data/train/{stock_symbol}_{start_date}_to_{end_date}.png')
 plt.close()
 
 # %%
-# Load the image
-image = cv2.imread(f'./mnt/data/{stock_symbol}_{start_date}_to_{end_date}.png')
+## IMAGE PREPROCESSING
+
+# Load and process the image
+image_path = f'./mnt/data/train/{stock_symbol}_{start_date}_to_{end_date}.png'
+image = cv2.imread(image_path)
 
 x_start, y_start = 130, 80 
 x_end, y_end = 900, 530
@@ -33,10 +38,33 @@ x_end, y_end = 900, 530
 # Crop the image
 cropped_image = image[y_start:y_end, x_start:x_end]
 
-# grayscale the image
+# Convert the cropped image to grayscale
 grayscale_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
 
-# Save or display the cropped image
-cv2.imwrite(f'./mnt/data/{stock_symbol}_{start_date}_to_{end_date}.png', grayscale_image)
+# Save the grayscale image back to the original path
+cv2.imwrite(image_path, grayscale_image)
+
+# Display the grayscale image
+plt.imshow(grayscale_image, cmap='gray')
+plt.show()
+
+# Path to the CSV file
+csv_file_path = './mnt/data/labels.csv'
+
+# Check if the CSV file exists; if not, create it and write the header
+if not os.path.isfile(csv_file_path):
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['filename', 'label'])
+
+# Set the label for the image (False for non-impulse and True for impulse)
+is_impulse = False 
+
+
+# Append the new image data to the CSV file
+with open(csv_file_path, mode='a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([os.path.basename(image_path), int(is_impulse)])
+
 
 
